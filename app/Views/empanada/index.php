3e9504352a0bbce3cd8147202df09510f1de558f
manuel2112 <manuel2112@hotmail.com>
2025-09-10 11:55:01 -0300

    feat: data update

--- app/Views/empanada/index.php
+++ app/Views/empanada/index.php
@@ -31,7 +31,7 @@
     </div>
 </main>
 
-<!-- Modal -->
+<!-- Modal Store -->
 <div class="modal fade" id="addEmpanadaModal" tabindex="-1" aria-labelledby="addEmpanadaModal" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -62,7 +62,57 @@
                         <span class="text-danger" id="fillingFormError"></span>
                     </div>
                     <div class="form-group d-flex justify-content-end mt-2">
-                        <button type="button" class="btn btn-primary" onclick="return storeData()">Guardar</button>
+                        <button type="button" class="btn btn-primary" onclick="storeData()">Guardar</button>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Update -->
+<div class="modal fade" id="updateEmpanadaModal" tabindex="-1" aria-labelledby="updateEmpanadaModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Actualizar empanada</h1>
+                <button type="button" class="btn-close" id="close-modal-update" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="updateForm">
+                    <input type="hidden" id="idUpdateForm">
+                    <div class="form-group">
+                        <label for="nameUpdateForm">Nombre</label>
+                        <input type="text" class="form-control" id="nameUpdateForm" placeholder="Nombre..." required>
+                        <span class="text-danger" id="nameUpdateFormError"></span>
+                    </div>
+                    <div class="form-group mt-2">
+                        <label for="typeUpdateForm">Tipo</label>
+                        <input type="text" class="form-control" id="typeUpdateForm" placeholder="Horno, frita, etc..." required>
+                        <span class="text-danger" id="typeUpdateFormError"></span>
+                    </div>
+                    <div class="form-group mt-2">
+                        <label for="priceUpdateForm">Precio</label>
+                        <input type="number" class="form-control" id="priceUpdateForm" placeholder="Precio..." required>
+                        <span class="text-danger" id="priceUpdateFormError"></span>
+                    </div>
+                    <div class="form-group mt-2">
+                        <label for="fillingUpdateForm">Relleno</label>
+                        <textarea class="form-control" id="fillingUpdateForm" rows="3" required></textarea>
+                        <span class="text-danger" id="fillingUpdateFormError"></span>
+                    </div>
+                    <div class="form-group mt-2">
+                        <input type="radio" class="btn-check" name="options-outlined" id="success-outlined" value="false" autocomplete="off">
+                        <label class="btn btn-outline-success" for="success-outlined">Disponible</label>
+
+                        <input type="radio" class="btn-check" name="options-outlined" id="danger-outlined" value="true" autocomplete=" off">
+                        <label class="btn btn-outline-danger" for="danger-outlined">Agotado</label>
+                    </div>
+                    <div class="form-group d-flex justify-content-end mt-2">
+                        <button type="button" class="btn btn-primary" onclick="updateData()">Actualizar</button>
                     </div>
                 </form>
             </div>
--- public/js/process.js
+++ public/js/process.js
@@ -1,3 +1,13 @@
+    
+
+    function formatMoneyString(value){
+        if (!value) return;
+        const formatValue = Number(value.split(".")[0]);
+        const newString = formatValue > 0 ? Number(value.split(".")[0]) : "$0";
+
+        return new Intl.NumberFormat('de-DE').format(newString);
+    };
+
     async function getList() {
         const url = "http://localhost:3000/api/empanadas/";
         try {
@@ -7,7 +17,6 @@
             }
 
             const data = await response.json();
-            console.log(data);
 
             const tableBody = document.getElementById('dataTableBody');
             tableBody.innerHTML = ''; // Clear existing content
@@ -28,7 +37,7 @@
                 tr.appendChild(td3);
 
                 const td4 = document.createElement('td');
-                td4.textContent = `$${row.price}`;
+                td4.textContent = `$${formatMoneyString(row.price)}`;
                 tr.appendChild(td4);
 
                 const td5 = document.createElement('td');
@@ -40,7 +49,7 @@
                 tr.appendChild(td6);
 
                 const td7 = document.createElement('td');
-                td7.innerHTML = `<div class="btn-group"><button class="btn btn-warning" onclick="getData(${row.id})"><i class="fa-solid fa-pen-to-square"></i></button> <button class="btn btn-danger" onclick="deleteData(${row.id})"><i class="fa-solid fa-trash"></i></button></div>`;
+                td7.innerHTML = `<div class="btn-group"><button class="btn btn-warning" onclick="getData(${row.id})" data-bs-toggle="modal" data-bs-target="#updateEmpanadaModal"><i class="fa-solid fa-pen-to-square"></i></button> <button class="btn btn-danger" onclick="deleteData(${row.id})"><i class="fa-solid fa-trash"></i></button></div>`;
                 tr.appendChild(td7);
 
                 tableBody.appendChild(tr);
@@ -160,9 +169,81 @@
             }
 
             const data = await response.json();
-            console.log(data);
+            document.getElementById('idUpdateForm').value = data[0].id;
+            document.getElementById('nameUpdateForm').value = data[0].name;
+            document.getElementById('typeUpdateForm').value = data[0].type;
+            document.getElementById('priceUpdateForm').value = data[0].price.split(".")[0];
+            document.getElementById('fillingUpdateForm').value = data[0].filling;
+
+            const radioToSelect = data[0].is_sold_out ? document.getElementById("danger-outlined") : document.getElementById("success-outlined");
+            if (radioToSelect) {
+                radioToSelect.checked = true;
+            }
         } catch (error) {
             console.error(error.message);
+        }
+    }
+    
+    async function updateData() {
+        
+        const form = document.getElementById('updateForm');
+        const id = document.getElementById('idUpdateForm').value;
+        const name = document.getElementById('nameUpdateForm');
+        const type = document.getElementById('typeUpdateForm');
+        const price = document.getElementById('priceUpdateForm');
+        const filling = document.getElementById('fillingUpdateForm');
+        const isSoldOut = document.querySelector('input[name="options-outlined"]:checked').value === 'false' ? false : true;
+
+        let isValid = true;
+        
+        nameUpdateFormError.textContent = '';
+        typeUpdateFormError.textContent = '';
+        priceUpdateFormError.textContent = '';
+        fillingUpdateFormError.textContent = '';
+        
+        if (name.value.length < 3) {
+            nameUpdateFormError.textContent = 'Nombre superior a 3 caracteres.';
+            isValid = false;
+        }
+        if (type.value.length < 3) {
+            typeUpdateFormError.textContent = 'Tipo superior a 3 caracteres.';
+            isValid = false;
+        }
+        if (Number(price.value) <= 0) {
+            priceUpdateFormError.textContent = 'Precio debe ser mayor a cero.';
+            isValid = false;
+        }
+        if (filling.value.length < 10) {
+            fillingUpdateFormError.textContent = 'Relleno debe ser mayor a 10 caracteres.';
+            isValid = false;
+        }
+
+        if (isValid) {
+            const updateData = {
+                name: name.value,
+                type: type.value,
+                price: Number(price.value),
+                filling: filling.value,
+                is_sold_out: isSoldOut
+            };
+            const response = await fetch(`http://localhost:3000/api/empanadas/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(updateData)
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            console.log('Success:', result);
+            document.getElementById("close-modal-update").click();
+            form.reset();
+            Swal.fire("Empanada actualizada", "La empanada ha sido actualizada exitosamente.", "success");
+            getList();
         }
     }
     
